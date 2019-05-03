0f2c5c97b146080ef3fa08c4ef6456c2db231959
ZaBralow <pavlik.elsukov@mail.ru>
2019-05-03 21:05:30 +0300

    okna8.1

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-$(document).ready(function () { // Ждём загрузки страницы
+$(document).ready(function () { 
 
-	$(".image").click(function () {	// Событие клика на маленькое изображение
-		var img = $(this);	// Получаем изображение, на которое кликнули
-		var src = img.attr('src'); // Достаем из этого изображения путь до картинки
-		$("body").append("<div class='popup'>" + //Добавляем в тело документа разметку всплывающего окна
-			"<div class='popup_bg'></div>" + // Блок, который будет служить фоном затемненным
-			"<img src='" + src + "' class='popup_img' />" + // Само увеличенное фото
+	$(".image").click(function () {
+		var img = $(this);	
+		var src = img.attr('src'); 
+		$("body").append("<div class='popup'>" +
+			"<div class='popup_bg'></div>" + 
+			"<img src='" + src + "' class='popup_img' />" +
 			"</div>");
-		$(".popup").fadeIn(300); // Медленно выводим изображение
-		$(".popup_img").click(function () {	// Событие клика на затемненный фон	   
-			$(".popup").fadeOut(300);	// Медленно убираем всплывающее окно
-			setTimeout(function () {	// Выставляем таймер
-				$(".popup").remove(); // Удаляем разметку всплывающего окна
+		$(".popup").fadeIn(300); 
+		$(".popup_img").click(function () {
+			$(".popup").fadeOut(300);	
+			setTimeout(function () {	
+				$(".popup").remove(); 
 			}, 300);
 		});
 	});
